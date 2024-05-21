6ada2ce0c3b751e7474dfc0995fdd0a34539170a
Sahir Sujahudeen <m.s.sujahudeen@student.tudelft.nl>
2024-05-21 11:11:41 +0200

    Added images

--- piv_code/Main.m
+++ piv_code/Main.m
@@ -1,19 +1,19 @@
 clc; clear all; close all;
 
-I = imread("PIV Samples\15deg\B00001.tif");
+I = imread("..\PIV_DATA\\15deg\B00001.tif");
 I = im2double(I);
 sizeI = size(I);
 
 image1 = I(1:sizeI(1)/2,:);
 image2 = I(sizeI(1)/2+1:end,:);
 
-image1 = imgaussfilt(image1, 5);
-image2 = imgaussfilt(image2, 5);
+image1 = imgaussfilt(image1, 1);
+image2 = imgaussfilt(image2, 1);
 
-% image1 = image1 - mean(image1(:));
-% image2 = image2 - mean(image2(:));
+image1 = image1 - mean(image1(:));
+image2 = image2 - mean(image2(:));
 
-windowSize = 16; % Size of the interrogation window
+windowSize = 2; % Size of the interrogation window
 step = windowSize; % Step size for window overlap
 
 % Preallocate vectors for displacement
@@ -60,7 +60,7 @@ for i = rowRange
         subImage2 = subImage2 - mean(subImage2);
 
         % Cross-correlation
-        correlation = xcorr2(subImage1,subImage2);
+        correlation = xcorr2_fft(subImage1,subImage2);
         
         % Find the peak in the correlation matrix
         [~, maxIndex] = max(correlation(:));
@@ -69,7 +69,7 @@ for i = rowRange
         % Calculate displacement
         u((i-1)/step+1, (j-1)/step+1) = peakCol;
         v((i-1)/step+1, (j-1)/step+1) = peakRow;
-        [i j]
+        [i j];
     end
 end
 
@@ -81,15 +81,15 @@ velocityY = v; %* scalingFactor / timeInterval;
 totalVelocity = sqrt(velocityX.^2 + velocityY.^2);
 % [X, Y] = meshgrid(1:step:cols, 1:step:rows);
 % quiver(velocityX, velocityY);
-contourf(velocityX,"LineStyle","none")
-% colorbar
+% contourf(velocityX,"LineStyle","none")
+colorbar
 % %%
 % figure
 % % velocityX(20,13) = NaN;
 % % velocityX(18,13) = NaN;
-% totalVelocity(:,end) = NaN;
-% totalVelocity(end,:) = NaN;
-% contourf(totalVelocity,"LineStyle","none")
-% % caxis([10, 30])
-% colormap parula
-% colorbar
+totalVelocity(:,end) = NaN;
+totalVelocity(end,:) = NaN;
+contourf(totalVelocity,"LineStyle","none")
+% caxis([10, 30])
+colormap parula
+colorbar
