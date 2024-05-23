5b46131018f5e710f7e76b863af3147f388bfab7
Glebdoc <gauja71@gmail.com>
2024-05-23 11:10:54 +0200

    main edited

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from scipy.ndimage import median_filter
 
 
 tif_array = tiff.imread("B00001.tif")
+win_size = 16
 
 twoimages = np.array(tif_array)
 img1 = twoimages[:twoimages.shape[0]//2, :]
@@ -15,51 +16,56 @@ img2 = twoimages[twoimages.shape[0]//2 :, : ]
 delta_t = 6e-6
 pix_density = 81_000
 
-def replace_outliers(data, win_size=3, threshold=1.3):
-    count = 0
-    z = stats.zscore(data, nan_policy='omit')
-    data_out = data.copy()
-    for i in range(data.shape[0]):
-        for j in range(data.shape[1]):
-            if z[i, j] > threshold:
-                # Calculate local mean
-                count += 1
-                local_mean = np.mean(data[max(0, i-win_size):min(data.shape[0], i+win_size+1),
-                                              max(0, j-win_size):min(data.shape[1], j+win_size+1)])
-                data_out[i, j] = local_mean
-    print('Total outliers removed:', count)
-    return data_out
+# def replace_outliers(data, win_size=3, threshold=1.3):
+#     count = 0
+#     z = stats.zscore(data, nan_policy='omit')
+#     data_out = data.copy()
+#     for i in range(data.shape[0]):
+#         for j in range(data.shape[1]):
+#             if z[i, j] > threshold:
+#                 # Calculate local mean
+#                 count += 1
+#                 local_mean = np.mean(data[max(0, i-win_size):min(data.shape[0], i+win_size+1),
+#                                               max(0, j-win_size):min(data.shape[1], j+win_size+1)])
+#                 data_out[i, j] = local_mean
+#     print('Total outliers removed:', count)
+#     return data_out
 
-def vel_field(img1, img2, win_size):
+
+def convolution(img1, img2, win_size):
     ys = np.arange(0, img1.shape[0], win_size)
     xs = np.arange(0, img1.shape[1], win_size)
     dys = np.zeros((len(ys), len(xs)))
     dxs = np.zeros((len(ys), len(xs)))
+    
     for iy, y in enumerate(ys):
         for ix, x in enumerate(xs):
-            int_win = img1[y : y + win_size, x : x + win_size]
-            search_win = img2[y : y + win_size, x : x + win_size]
+            int_win = img1[y:y + win_size, x:x + win_size]
+            search_win = img2[y:y + win_size, x:x + win_size]
             cross_corr = correlate(search_win - search_win.mean(), int_win - int_win.mean(), method="fft")
-            dys[iy, ix], dxs[iy, ix] = (np.unravel_index(np.argmax(cross_corr), cross_corr.shape) - np.array([win_size, win_size]) + 1)
-    ys = ys + win_size / 2
-    xs = xs + win_size / 2
-    return xs, ys, dxs, dys
+            #cross_corr = correlate(search_win, int_win, method="fft")
+            max_pos = np.unravel_index(np.argmax(cross_corr), cross_corr.shape)
+            dy, dx = max_pos - np.array([win_size, win_size]) + 1
+            dys[iy, ix] = dy
+            dxs[iy, ix] = dx
+    
+    return dxs, dys
 
-
-
-xs, ys, dxs, dys = vel_field(img1, img2, 16)
+dxs, dys = convolution(img1, img2, win_size)
 
 u = dxs/delta_t/pix_density
 v = dys/delta_t/pix_density
+
 velocity_magnitude = np.sqrt(u**2 + v**2) 
+
 velocity_original = np.copy(velocity_magnitude)
 
-for i in range(10):
+for i in range(1):
     velocity_magnitude_filtered = median_filter(velocity_magnitude, size=3)
     velocity_magnitude = velocity_magnitude_filtered
 
 
-levels = [0.0001, 0.4, 0.7, 1, 2, 4, 6, 8, 10, 11, 12, 14, 16,18,20]
+levels = [ -.5, 0.0, 0.4, 0.7, 1, 2, 4, 6, 8, 10, 11, 12, 14, 16,18,20]
 
 
 fig, ax = plt.subplots(1, 2, figsize=(10, 5))
